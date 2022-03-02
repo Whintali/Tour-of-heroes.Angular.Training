e9920ab35a26fab0135439b317adae4009757e1c
Whintali <whintali@gmail.com>
2022-03-02 17:26:44 +0100

    1 file modified (Pipe not finished)

--- src/app/service/others/test.pipe.ts
+++ src/app/service/others/test.pipe.ts
@@ -1,12 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Hero } from 'src/app/heroes';
 
 @Pipe({
   name: 'test'
 })
-export class TestPipe {
-  damame_dealt?:number;
-  construtor(args: TestPipe = {}) {
-    
+export class TestPipe implements PipeTransform {
+  transform(value: number, damage_dealt?: number): Hero{
+      if(this.damage_dealt > this.value){
+          return Hero
+      }
   }
 
 }
